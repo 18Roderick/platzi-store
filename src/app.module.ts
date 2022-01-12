1c32ef7e45e5f0fa81a1831a23d4ecb66c457013
Roderick Romero <rjrr507@gmail.com>
2022-01-11 21:14:50 -0500

    ajustes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+import * as morgan from 'morgan';
 @Module({
   imports: [ProductsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(morgan('dev'));
+  }
+}
